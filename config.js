e6e3791b1c75ca8cede405ae11107e31f92d7c8e
598416892 <598416892@qq.com>
2019-08-15 16:45:55 +0800

    test

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 var glConfig = {
+  baseURL:'https://cdn.m.tencent.com/hr/h5/2019/819/',
   requestUrl: './assets/luck_draw_info.json', //1.获取当前用户的抽奖信息
   luckDraw: './assets/luck_draw.json' //2.	抽奖接口
 };
