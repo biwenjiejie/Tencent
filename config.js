b2348a3afe16ba60526e5048e6cf3718ee68ff5f
598416892 <598416892@qq.com>
2019-08-15 11:17:11 +0800

    20190815

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 var glConfig = {
-  requestUrl: './assets/luck_draw_info.json' //本地
+  requestUrl: './assets/luck_draw_info.json', //1.获取当前用户的抽奖信息
+  luckDraw: './assets/luck_draw.json' //2.	抽奖接口
 };
